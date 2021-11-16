d39f7ceb2dbf2fef46df68b6c922335fddabc811
thescientist <the-scientist@rootstorm.com>
2021-11-16 14:52:55 -0700

    Arrays of string identifiers should be globals. (#9)

---
+++ include/strings_global.h
@@ -0,0 +1,14 @@
+#ifndef STRINGS_GLOBAL_H
+#define STRINGS_GLOBAL_H
+
+#include <elf.h>
+
+// in elfread.c
+extern const char* elf_class_id[ELFCLASSNUM];
+extern const char* elf_data_id[ELFDATANUM];
+extern const char* elf_osabi_id[];
+extern const char* elf_e_type_id[];
+extern const char* elf_e_machine_id[EM_NUM];
+extern const char* elf_e_version_id[EV_NUM];
+
+#endif // STRINGS_GLOBAL_H;
